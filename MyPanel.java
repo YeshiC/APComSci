2dbc05ce0d7926a4a9ff169dc0a920417fe5ee45
YeshiC <yeshic9@gmail.com>
2015-05-23 17:40:49 -0500

    fixed acceleration for now

--- MyPanel.java
+++ MyPanel.java
@@ -1,39 +1,60 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
-
+import java.awt.Color;
 public class MyPanel extends JPanel {
     private static final int IFW = JComponent.WHEN_IN_FOCUSED_WINDOW;
+    //Controls for Horizantal Movement
     private static final String FIRE = "player fire";
     private static final String FIRE2 = "player2 fire";
+    private static final String RELEASED = "player3 fire";
+    private static final String RELEASED2 = "player4 fire";
+    //Cordinates
     int x = 100;
     int y = 100;
     double dx =0, dy =0;
-    static JLabel obj1 = new JLabel("Welcome to MARIO");
+    //Dimensions for the frame
+    int width = 800;
+    int height = 300;
     
+    //Check to reset accelration
+    boolean reverse = true;
+    
+    //Misc
+    static JLabel obj1 = new JLabel("Welcome to MARIO");
+    Color backG = Color.BLACK;
     private static ImageIcon ii = new ImageIcon("Mario.png");
     public MyPanel() {
-       // ii = new ImageIcon("Mario.png");
-        setPreferredSize(new Dimension(800,300));
-        
+        //sets background and dimensoins
+        setPreferredSize(new Dimension(width,height));
+        setBackground(backG);
         this.add(obj1);
-        //this.setBounds(20,20,200,200);
+       
+        if(x!= 0 && x != width)
         dx -= .1;
+        
+        //Input Maps
         this.getInputMap(IFW).put(KeyStroke.getKeyStroke("LEFT"), FIRE);
         this.getInputMap(IFW).put(KeyStroke.getKeyStroke("RIGHT"), FIRE2);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("released LEFT"), RELEASED);
+        this.getInputMap(IFW).put(KeyStroke.getKeyStroke("released LEFT"), RELEASED2);
         this.getActionMap().put(FIRE, new FireAction(1));
         this.getActionMap().put(FIRE2, new FireAction2(1));
+        this.getActionMap().put(RELEASED, new ReleaseAction(1));
+        this.getActionMap().put(RELEASED2, new ReleaseAction2(1));
+        
 
     }
-
+    
     @Override
+    //Paints the sprite
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         ii.paintIcon(this,g,x,y); 
         // g.dispose();
     }
-    
+
     public int getY()
     {
         return y;
@@ -43,18 +64,12 @@ public class MyPanel extends JPanel {
     {
         return x;
     }
-    /*static void rebindKey(KeyEvent ke, String oldKey) {
-    //      Depends on your GUI implementation.
-    //      Detecting the new key by a KeyListener is the way to go this time.
-    obj1.getInputMap(IFW).remove(KeyStroke.getKeyStroke(oldKey));
-    //      Removing can also be done by assigning the action name "none".
-    obj1.getInputMap(IFW).put(KeyStroke.getKeyStrokeForEvent(ke),
-    obj1.getInputMap(IFW).get(KeyStroke.getKeyStroke(oldKey)));
-    //      You can drop the remove action if you want a secondary key for the action.
-    }*/
 
+    //CONTOLS MOVEMENT------------------------------------------------------------------
+    //**********************************************************************************
+    //**********************************************************************************
     
-
+    //Actions for button pressed
     private class FireAction extends AbstractAction {
         int player;
         FireAction(int player) {
@@ -64,25 +79,63 @@ public class MyPanel extends JPanel {
         @Override
         public void actionPerformed(ActionEvent e) {
             dx += .2;
+            if(reverse == true)
+            {
+                dx = 0;
+                reverse = false;
+            }
+             if(x <= 0)
+            {
+                dx =0;
+                x = 0;
+            }
             x -= (int)dx;
-            //System.out.println(x);   
+
             repaint();
         }
     }
     private class FireAction2 extends AbstractAction {
-
         int player;
-
         FireAction2(int player) {
             this.player = player;
         }
-
         @Override
         public void actionPerformed(ActionEvent e) {       
             dx += .2;
+            if(reverse == false)
+            {
+                dx = 0;
+                reverse = true;
+            }
+            if(x >= width)
+            {
+                dx = 0;
+                x = width;
+            }
             x += (int)dx;
-            //System.out.println(x);  
             repaint();
+        }
+    }
+    
+    //Accelration Reset
+    private class ReleaseAction extends AbstractAction {
+        int player;
+        ReleaseAction(int player) {
+            this.player = player;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {       
+            dx = 0;
+        }
+    }
+    private class ReleaseAction2 extends AbstractAction {
+        int player;
+        ReleaseAction2(int player) {
+            this.player = player;
+        }
+        @Override
+        public void actionPerformed(ActionEvent e) {       
+            dx = 0;
         }
     }
 }
